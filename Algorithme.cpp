e2ea136111150164f251a81fb28df2618ef3208c
Lucien775 <lucien.coudert.pro@sfr.fr>
2025-12-04 11:43:53 +0100

    Debug

--- Algorithme.cpp
+++ Algorithme.cpp
@@ -7,13 +7,13 @@ template <size_t N>
 class Optimiseur
 {
 protected: 
-    FonctionObjective& f;
+    FonctionObjectif<N>& f;
     double alpha;
     double epsilon;
     int max_iters;
 
 public:
-    Optimiseur(FonctionObjective& func, double a, double e, int max_it)
+    Optimiseur(FonctionObjectif<N>& func, double a, double e, int max_it)
         : f(func), alpha(a), epsilon(e), max_iters(max_it) {}
 
     virtual ~Optimiseur() = default;
@@ -26,7 +26,7 @@ public:
             double fx = f.evaluer(x);
 
             auto grad = f.calculerGradient(x);
-            double grad_norm = grad.norm();
+            double grad_norm = grad.calculNorm();
 
             if (grad_norm < epsilon)
             {
@@ -45,11 +45,11 @@ public:
 
 
 template <size_t N>
-class DescenteGradient : public Optimiseur
+class DescenteGradient : public Optimiseur<N>
 {
 public:
-    DescenteGradient(FonctionObjective& func, double a, double e, int max_it)
-        : Optimiseur(func, a, e, max_it) {}
+    DescenteGradient(FonctionObjectif<N>& func, double a, double e, int max_it)
+        : Optimiseur<N>(func, a, e, max_it) {}
 
     ~DescenteGradient() override = default;
     Vecteur<N> calculerDirection(const Vecteur<N>& x) const override
@@ -60,17 +60,17 @@ public:
 };
 
 template <size_t N>
-class PlusFortePente : public Optimiseur
+class PlusFortePente : public Optimiseur<N>
 {
 public:
-    PlusFortePente(FonctionObjective& func, double a, double e, int max_it)
-        : Optimiseur(func, a, e, max_it) {}
+    PlusFortePente(FonctionObjectif<N>& func, double a, double e, int max_it)
+        : Optimiseur<N>(func, a, e, max_it) {}
 
     ~PlusFortePente() override = default;
     Vecteur<N> calculerDirection(const Vecteur<N>& x) const override
     {
         auto g = f.calculerGradient(x);
-        double g_norm = g.norm();
+        double g_norm = g.calculNorm();
         double coeff = (-1.0) / g_norm;
         return g * coeff;   
     }
@@ -82,10 +82,10 @@ int main(){
 	double alpha = 0.1;
     double epsilon = 10e-6;
 	
-    FonctionObjective f1;
+    FonctionObjectif1 f1;
 	DescenteGradient dg(f1, alpha, epsilon, 100);
-    Vecteur x0 = {0.0, 0.0}
-	DescenteGradient.optimiser(x0);
+    Vecteur<2> x0 = {0.0, 0.0};
+	dg.optimiser(x0);
     
 
     return 0;
